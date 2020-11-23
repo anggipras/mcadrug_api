d07d093a70f4bc9d23febdb7d1f9424d9d403718
anggi <anggiprastianto30@gmail.com>
2020-11-23 18:26:44 +0700

    store personal data

--- src/connections/mysqldb.js
+++ src/connections/mysqldb.js
@@ -1,18 +1,10 @@
 const mysql = require('mysql')
 
-// const db = mysql.createConnection({
-//     host        : process.env.DB_HOST,
-//     user        : process.env.DB_USER,
-//     password    : process.env.DB_PASS,
-//     database    : process.env.DB_DATABASE,
-//     port        : 3306
-// })
-
 const db = mysql.createConnection({
-    host        : 'db4free.net',
-    user        : 'anggipras',
-    password    : 'Godislove30~',
-    database    : 'mcadstore',
+    host        : process.env.DB_HOST,
+    user        : process.env.DB_USER,
+    password    : process.env.DB_PASS,
+    database    : process.env.DB_DATABASE,
     port        : 3306
 })
 
